32db237ed1db61f55b0e209c4e5f938cdc9de77d
Dhananjay <dshitkar@pdx.edu>
2022-11-28 18:56:55 -0800

    temp

---
+++ src/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import App from "./App";
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
+    return (
+        <div className="auth-form-container">
+            <img src="psu2.png"></img>
+            <h2>Log into your HermPDX account</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="Email">Email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                <label htmlFor="Password">Password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                <button type="Submit" onClick={App}>Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
